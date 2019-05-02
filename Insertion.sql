e5c0bc1064fd7146e35ced87f594ae44ce67576a
Faruk Aygün <farukaygun@live.com>
2019-05-02 19:28:06 +0300

    Add files via upload

---
+++ Insertion.sql
@@ -0,0 +1,219 @@
+--İsimler Visual Studio Code eklentisi Faker ile rastgele olarak oluşturuldu.
+
+USE BoardGameArchive
+
+INSERT INTO tblYayimci
+VALUES ('Olson, Schulist and Ledner', 'mail1', '2012-06-18 10:34:09', 'website1')
+
+INSERT INTO tblYayimci
+VALUES ('Ratke - Gleason', 'MAİL2', '2012-06-18 10:34:09', 'website2')
+
+INSERT INTO tblYayimci
+VALUES ('Miller - Zulauf', 'mail3', '2012-06-18 10:34:09', 'website3')
+
+INSERT INTO tblYayimci
+VALUES ('Welch Group', 'mail4', '2012-06-18 10:34:09', 'website4')
+
+INSERT INTO tblOyun
+VALUES ('oyun1', 7.7, '2012-06-18 10:34:09', 9, null, null, null, null, 18, 'aciklana', 1)
+
+INSERT INTO tblOyun
+VALUES ('oyun2', 10, '2012-06-18 10:34:09', 6, null, null, null, null, 17, 'aciklama2', 2)
+
+INSERT INTO tblOyun
+VALUES ('oyun3', 5.5, '2012-06-18 10:34:09', 6, null, null, null, null, 12, 'aciklama3', 4)
+
+INSERT INTO tblOyun
+VALUES ('oyun4', 3.5, '2012-06-18 10:34:09', 4, null, null, null, null, 8, 'aciklama4', 4)
+
+INSERT INTO tblOyuncu
+VALUES ('Luther Stark', 1)
+
+INSERT INTO tblOyuncu
+VALUES ('Darius Wyman', 0)
+
+INSERT INTO tblOyuncu
+VALUES ('Gregory Howe', 0)
+
+INSERT INTO tblOyuncu
+VALUES ('Ed Ortiz', 0)
+
+INSERT INTO tblLokasyon
+VALUES ('ev', 'Edwin Ridge 796 Herman Extension Apt. 264')
+
+INSERT INTO tblLokasyon
+VALUES ('otel', 'Zula Forest 3211 Rowland Ferry Suite 959')
+
+INSERT INTO tblLokasyon
+VALUES ('kulüp', 'Chanelle Cliff 868 Stanton Island Apt. 009')
+
+INSERT INTO tblLokasyon
+VALUES ('okul', 'Bradtke Estates 47324 Abshire Squares Suite 441')
+
+INSERT INTO tblOyna
+VALUES ()
+
+INSERT INTO tblOyna
+VALUES ()
+
+INSERT INTO tblOyna
+VALUES ()
+
+INSERT INTO tblOyna
+VALUES ()
+
+-- INSERT INTO tblRozet
+-- VALUES ()
+
+-- INSERT INTO tblRozet
+-- VALUES ()
+
+-- INSERT INTO tblRozet
+-- VALUES ()
+
+-- INSERT INTO tblRozet
+-- VALUES ()
+
+-- INSERT INTO tblGruplar
+-- VALUES ()
+
+-- INSERT INTO tblGruplar
+-- VALUES ()
+
+-- INSERT INTO tblGruplar
+-- VALUES ()
+
+-- INSERT INTO tblGruplar
+-- VALUES ()
+
+-- INSERT INTO tblIl
+-- VALUES ()
+
+-- INSERT INTO tblIl
+-- VALUES ()
+
+-- INSERT INTO tblIl
+-- VALUES ()
+
+-- INSERT INTO tblIl
+-- VALUES ()
+
+-- INSERT INTO tblIlce
+-- VALUES ()
+
+-- INSERT INTO tblIlce
+-- VALUES ()
+
+-- INSERT INTO tblIlce
+-- VALUES ()
+
+-- INSERT INTO tblIlce
+-- VALUES ()
+
+-- INSERT INTO tblArtist
+-- VALUES ()
+
+-- INSERT INTO tblArtist
+-- VALUES ()
+
+-- INSERT INTO tblArtist
+-- VALUES ()
+
+-- INSERT INTO tblArtist
+-- VALUES ()
+
+-- INSERT INTO tblTasarimci
+-- VALUES ()
+
+-- INSERT INTO tblTasarimci
+-- VALUES ()
+
+-- INSERT INTO tblTasarimci
+-- VALUES ()
+
+-- INSERT INTO tblTasarimci
+-- VALUES ()
+
+-- INSERT INTO tblOdul
+-- VALUES ()
+
+-- INSERT INTO tblOdul
+-- VALUES ()
+
+-- INSERT INTO tblOdul
+-- VALUES ()
+
+-- INSERT INTO tblOdul
+-- VALUES ()
+
+-- INSERT INTO tblKategori
+-- VALUES ()
+
+-- INSERT INTO tblKategori
+-- VALUES ()
+
+-- INSERT INTO tblKategori
+-- VALUES ()
+
+-- INSERT INTO tblKategori
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuYorumYaparOyun
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuYorumYaparOyun
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuYorumYaparOyun
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuYorumYaparOyun
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuSatinAlirOyun
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuSatinAlirOyun
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuSatinAlirOyun
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuSatinAlirOyun
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuArkadasOlur
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuArkadasOlur
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuArkadasOlur
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuArkadasOlur
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuKatilirOyna
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuKatilirOyna
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuKatilirOyna
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuKatilirOyna
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuSatinAlirRozet
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuSatinAlirRozet
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuSatinAlirRozet
+-- VALUES ()
+
+-- INSERT INTO tblOyuncuSatinAlirRozet
+-- VALUES ()
